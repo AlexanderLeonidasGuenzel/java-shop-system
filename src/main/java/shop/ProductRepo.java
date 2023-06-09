23316df467bc0877675fdb853ab45cba2a1d9b82
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-06-09 15:05:31 +0200

    ProductRepoTest Test for list() and getProductByName()

--- src/main/java/shop/ProductRepo.java
+++ src/main/java/shop/ProductRepo.java
@@ -1,9 +1,27 @@
 package shop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductRepo {
+
+    private Map<String, Product> products;
+
     public ProductRepo(Product... productArray) {
+        this.products = new HashMap<>();
+        for (Product product: productArray
+             ) {
+             products.put(product.getName(),product);
+        }
     }
 
+     public Map<String, Product> list(){
+        return products;
+    }
+
+    public Product getProductByName(String key){
+        return products.get(key);
+    }
 
 
 }
--- src/test/java/shop/ProductRepoTest.java
+++ src/test/java/shop/ProductRepoTest.java
@@ -9,19 +9,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepoTest {
 
-    private Product p1 = new Product("378","apple");
-    private Product p2 = new Product("375","banana");
-    private Product p3 = new Product("379","shoe");
-    private Product p4 = new Product("370","t-shirt");
+    @Test
+    void yields_ListOfAllProductsInProductRepo(){
 
-    private ProductRepo pr1 = new ProductRepo(p1, p2, p3, p4);
+         Product p1 = new Product("378","apple");
+         Product p2 = new Product("375","banana");
+         Product p3 = new Product("379","shoe");
+         Product p4 = new Product("370","t-shirt");
+
+         ProductRepo pr1 = new ProductRepo(p1, p2, p3, p4);
+
+         Map<String, Product> products = new HashMap<>();
+        products.put(p1.getName(),p1);
+        products.put(p2.getName(),p2);
+        products.put(p3.getName(),p3);
+        products.put(p4.getName(),p4);
+
+        assertEquals(products,pr1.list());
+    }
 
     @Test
-    void yieldsAllListOfProducts(){
-        Map<String, Product> list = new HashMap<>();
-
-        assertEquals();
-
+    void yields_ProductFromInsertedKey(){
+        Product p1 = new Product("378","apple");
+        ProductRepo pr1 = new ProductRepo(p1);
+        assertEquals(p1, pr1.getProductByName("apple"));
     }
 
 }
